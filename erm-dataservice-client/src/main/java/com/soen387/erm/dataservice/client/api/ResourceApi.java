083b375b2c9b14d7eee36c6b695a95c3e75998e6
daliaivy17 <ivyyotha17@gmail.com>
2016-11-24 15:12:01 -0500

    added code for client resource

---
+++ erm-dataservice-client/src/main/java/com/soen387/erm/dataservice/client/api/ResourceApi.java
@@ -0,0 +1,21 @@
+package com.soen387.erm.dataservice.client.api;
+
+import com.soen387.erm.dataservice.common.model.BaseEntity;
+import com.soen387.erm.dataservice.common.model.auth.Department;
+import com.soen387.erm.dataservice.client.jaxrs.HalResource;
+import com.soen387.erm.dataservice.client.jaxrs.RestClient;
+import org.springframework.hateoas.Resource;
+
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.Response;
+import java.util.Collection;
+
+
+public class ResourceApi extends BaseApe<Resource>{
+
+
+
+
+	
+}
--- erm-dataservice-client/src/main/java/com/soen387/erm/dataservice/client/model/resource/AbstractResource.java
+++ erm-dataservice-client/src/main/java/com/soen387/erm/dataservice/client/model/resource/AbstractResource.java
@@ -3,10 +3,46 @@ package com.soen387.erm.dataservice.client.model.resource;
 import com.soen387.erm.dataservice.client.model.BaseEntity;
 
 /**
- * Created by jeremybrown on 2016-11-22.
  */
+
+@XmlRootElement(name = "resource")
+@JsonTypeName("resource")
+
 public class AbstractResource extends BaseEntity {
 
-    // TODO
+    private  long resourceID;
 
+    private String name;
+
+    private String description;
+
+    private Boolean available;
+
+
+    private Boolean moveable;
+
+    public String getName() {
+        return name;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public Boolean getMoveable() {
+        return moveable;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Long getResourceId() {
+        return resourceId;
+    }
+
+    @Override
+    public String toString() {
+        return "AbstractResource " + getName();
+    }
 }
